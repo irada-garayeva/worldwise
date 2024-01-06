2a59c16c8d044bba7eb4f3b7c9f2e40a6ae491d4
irada-garayeva <irada.garayeva98@gmail.com>
2024-01-06 16:49:57 +0100

    Creating a CitiesContext

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,9 +1,11 @@
 import CityItem from "./CityItem";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
 import styles from "./CityList.module.css";
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
